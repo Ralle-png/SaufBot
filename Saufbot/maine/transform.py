73c0423eec85062d2ef12bd5827858ae4e0ada31
lil-RAAN <loh_ralph@yahoo.de>
2022-03-24 12:52:19 +0100

    Transform funktioniert in 90% der Fällen

--- Saufbot/maine/transform.py
+++ Saufbot/maine/transform.py
@@ -1,18 +1,18 @@
 import re, json
 
-with open('dump.json', 'r') as dict, open('rezepte_test.json', 'r') as chg:
+with open('dump.json', 'r') as dict, open('rezepte_.json', 'r') as chg:
     dict = json.load(dict)
     dump = json.load(chg)
 
     for rezept in dump:
         for ing in dump[str(rezept)]['Zutaten']:
             ingr = dump[str(rezept)]['Zutaten'][ing]
-            print(ingr)
-            pattern = re.compile('[A-ZÄÖÜ][a-zäöüñçèéúß()%-_]+ ([A-ZÄÖÜ()][a-zäöüñçèéú%()]*)*')
+            #print(ingr)
+            pattern = re.compile('[A-Z][A-Za-zäöüñçèéúß%-_\s\(\)]+')
             matches = pattern.search(ingr)
             if(matches != None):
                 result = matches.span()
-                ingr = re.sub(ingr[matches.start() : matches.end()], 'A',dump[str(rezept)]['Zutaten'][ing])
+                ingr = re.sub(ingr[matches.start() : matches.end()], '',dump[str(rezept)]['Zutaten'][ing])
                 print(ingr)
 
 
