2511d6717335a7509635dc04af404d70dd5efcfc
lil-RAAN <loh_ralph@yahoo.de>
2022-04-05 03:22:22 +0200

    S T A B L E

--- Saufbot/guiApp/main.py
+++ Saufbot/guiApp/main.py
@@ -1,5 +1,7 @@
-from kivy.app import App
+from functools import partial
 import json
+from kivy.app import App
+from kivy.clock import mainthread
 from kivy.metrics import dp
 from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
@@ -12,36 +14,54 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.screenmanager import ScreenManager,Screen
 
 
-
-
-# class TestButton (Button):
-#     def __init__(self,**kwargs):
-#         super(TestButton, self).__init__(**kwargs)
 class MenuScreen(Screen):
     buchstabe = StringProperty('A')
+    titel = StringProperty('')
+    alkgehalt = StringProperty('')
+    zutaten = StringProperty('')
+
+
     vorne = {'A':'B','B':'C','C':'D','D':'E','E':'F','F':'G','G':'H','H':'I','I':'J','J':'K','K':'L','L':'M','M':'N','N':'O','O':'P','P':'Q','Q':'R','R':'S','S':'T','T':'U','U':'V','V':'W','W':'X','X':'Y','Y':'Z','Z':'A'}
     zuruck = {'A':'Z','B':'A','C':'B','D':'C','E':'D','F':'E','G':'F','H':'G','I':'H','J':'I','K':'J','L':'K','M':'L','N':'M','O':'N','P':'O','Q':'P','R':'Q','S':'R','T':'S','U':'T','V':'U','W':'V','X':'W','Y':'X','Z':'Y'}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
 
     def on_button_click(self,widget):
         if widget.text == '<-':
             self.buchstabe = self.zuruck[self.buchstabe]
         elif widget.text == '->':
             self.buchstabe = self.vorne[self.buchstabe]
-
         DrinkStack.buildpage(self, self.buchstabe)
 
-
-
-
+    def change_text(self, titel, alkgehalt, zutaten):
+        self.titel = titel
+        self.zutaten = zutaten
+        self.alkgehalt = alkgehalt
 
 class DrinkStack(StackLayout):
-
+    rezepte = {}
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        list2 = list()
+        with open('karte.json', 'r') as f:
+            listn = json.load(f)
+
+
+        search = 'A'
+        for i in listn:
+            if str(i).startswith(search):
+                list2.append(i)
+            f.close()
+        for i in list2:
+            self.add_widget(Button(text=str(i) ,size_hint=(1, None), on_press=partial(DrinkStack.change_content,self)))
 
 
 
-    def buildpage(self, value):
+    def buildpage(self ,value):
+
+        self.ids.ds1.clear_widgets()
         with open('karte.json', 'r') as f:
             listn = json.load(f)
         list2 = list()
@@ -51,21 +71,21 @@ class DrinkStack(StackLayout):
             if str(i).startswith(search):
                 list2.append(i)
         for i in list2:
-            self.add_widget(Button(text=str(i), size_hint=(1, None)))
+            btn = Button(text=str(i),size_hint=(1, None), on_press=partial(DrinkStack.change_content,self))
+            self.ids.ds1.add_widget(btn)
 
 
-    def nextpage(self):
-        pass
+    def change_content(self, instance):
+        with open('rezepte_.json', 'r') as f2:
+            rezepte = json.load(f2)
+
+        print(rezepte[instance.text]['AlkGehalt'])
+        s = ''
+        for i in container_rezepte.rezepte[instance.text]['Zutaten']:
+            s += container_rezepte.rezepte[instance.text]['Zutaten'][i] + '\n'
+        MenuScreen.change_text(self, titel=instance.text, alkgehalt=container_rezepte.rezepte[instance.text]['AlkGehalt'], zutaten=s)
 
 
-
-class ScreenManager(Screen):
-    def __init__(self, **kwargs):
-        super(ScreenManager, self).__init__(**kwargs)
-
-class StartScreen(Screen):
-    def __init__(self, **kwargs):
-        super(StartScreen, self).__init__(**kwargs)
 class MixerScreen(Screen):
     pass
 class LuckyScreen(Screen):
@@ -74,6 +94,16 @@ class KonfiScreen(Screen):
     pass
 class ManagerScreen(Screen):
     pass
+class StartScreen(Screen):
+    pass
+
+class Contaier():
+    rezepte = {}
+    def __init__(self):
+        with open('rezepte_.json', 'r') as f:
+            self.rezepte = json.load(f)
+        f.close()
+
 
 sm = ScreenManager()
 sm.add_widget(StartScreen(name='start'))
@@ -88,5 +118,5 @@ class guiApp(App):
         pass
 
 if __name__ == '__main__' :
-    ds = DrinkStack()
+    container_rezepte = Contaier()
     guiApp().run()
